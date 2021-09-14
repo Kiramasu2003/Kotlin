f276c21bd51d757044c4e6a913cba8c791949d4e
Kiramasu2003 <sarcer1800@alumno.ipn.mx>
2021-09-14 17:27:34 -0500

    DURI DURI DURI DURI DURI DURI DU

--- src/seccionDos/ArrayList.kt
+++ src/seccionDos/ArrayList.kt
@@ -8,9 +8,55 @@ package seccionDos
 fun main(){
 
     var esferasDragon = arrayListOf(1,2,3,4,5,6,7)
-    
+
+    println(esferasDragon[6])//vemos que valor hay en index:6
+    println(esferasDragon.size)//el tamaño del arraylist
+    esferasDragon.add(8)//añadir elementos al array, lo agrega en el ultimo indice
+    println(esferasDragon[7])//vemos que si lo agrego correctamente
+    println(esferasDragon.size)//el nuevo tamaño
+    esferasDragon.removeAt(7)//removemos el elemento en index:7
+   //vemos el nuevo tamaño despues de remover el elemento index:7
     println(esferasDragon.size)
-    esferasDragon.add(8)
-    println(esferasDragon.size)
+    esferasDragon[6]=8//reemplazamos el 7, por el 8
+
+
+    var abcedario= arrayListOf<Char>('z', 'h', 'q', 'b','a')
+    var numeros = arrayListOf<Int>(30,12,24,15,1232,0)
+    abcedario.sort()//reordenamos el array, se ordenara alfabeticamente, o
+    numeros.sort()
+
+    var counter = 0
+    for(letra in abcedario){
+        println("""
+            la letra $letra está en la posición $counter
+        """.trimIndent())
+        counter++
+
+    }
+
+    var counter2 = 0
+    for(numero in numeros){
+        println("""
+            el numero $numero está en la posición $counter2
+        """.trimIndent())
+        counter2++
+
+    }
+    //función lambda
+    abcedario.forEach{ letra ->
+        println("""
+            letra es $letra
+        """.trimIndent())
+    }
+
+    abcedario.forEachIndexed { i, letra ->
+
+        println("""
+            letra $letra está en la posición $i
+        """.trimIndent())
+    }
+
+
+
 
 }
